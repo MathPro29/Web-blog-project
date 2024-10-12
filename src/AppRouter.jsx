757dbf9b036f84d2ebe23764395661d4107e2a93
MathPro29 <mathelos8989@gmail.com>
2024-10-12 15:15:34 +0700

    details-changed

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,12 +4,15 @@ import About from './pages/About';
 
 function AppRouter() {
   return (
+    // เพิ่มเส้นทาง ให้เพิ่ม  <Link to="/...">ชื่อหัวข้อ</Link>
+    // สร้างไฟล์ในหน้า pages ด้วย
     <Router>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
       <Routes>
+        {/* อัปเดทเส้นทางให้เพิ่ม <Route path="/..." element={<ชื่อหัวข้อ />} /> */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
